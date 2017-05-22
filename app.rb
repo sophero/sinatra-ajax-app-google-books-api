9a41c1f1262b843ff4eeb39d339a5608c6d370df
Sophia Di <backwardsxmarathon@gmail.com>
2017-05-22 12:10:20 -0400

    Adds sign up, sign in and sign out functionality. Adds json gem to Gemfile.

--- app.rb
+++ app.rb
@@ -8,20 +8,30 @@ set :sessions, true
 require './models'
 
 get '/' do
+    redirect '/search' if session[:user_id]
     erb :index
 end
 
-post '/create_user' do
+get '/search' do
+    redirect '/' if session[:user_id] == nil
+    @user = User.find(session[:user_id])
+    erb :search
+end
 
+post '/create_user' do
+    user = User.create(params[:user])
+    session[:user_id] = user.id
+    redirect '/search'
 end
 
 post '/sign_in' do
-    # user = User
-    # if params[:password] ==
-    #
-    # end
+    user = User.where(username: params[:username])[0]
+    redirect '/' if user == nil || user.password != params[:password]
+    session[:user_id] = user.id
+    redirect '/search'
 end
 
-get '/search' do
-    erb :search
+post '/sign_out' do
+    session[:user_id] = nil
+    redirect '/'
 end
